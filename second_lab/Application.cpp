b33f3d01d5a7ae8d8818dcddf050ddc7477b66cf
k4r3l1ns <makcum.kyp4ehkob@gmail.com>
2023-10-10 15:12:31 +0300

    Minor code cleaning changes + reworked "main" structure according to lab task

--- second_lab/Application.cpp
+++ second_lab/Application.cpp
@@ -1,13 +1,23 @@
 #include <iostream>
 #include "image/Image.h"
 #include "service/ImageRefactoringUnit.h"
+#include <string>
+
+const static int SIZE = 5;
+const static double SIGMA = 1.2;
+const static std::string IMAGE_DIR_PATH = "../resources";
+
 
 int main() {
 
-    auto myImage = Image("../resources/image.png");
+    auto imageEven = Image(IMAGE_DIR_PATH + "/source/filtered/image_even.png");
+    auto imageGaussian = Image(IMAGE_DIR_PATH + "/source/filtered/image_even.png");
 
-    ImageRefactoringUnit::applyMedianFilter(myImage, 6);
-    myImage.isWrite("../resources/refactored_image.png");
+    ImageRefactoringUnit::applyMedianFilter(imageEven, SIZE);
+    ImageRefactoringUnit::applyGaussianFilter(imageGaussian, SIZE, SIGMA);
+
+    imageEven.isWrite( IMAGE_DIR_PATH + "/refactored/refactored_image_even.png");
+    imageGaussian.isWrite(IMAGE_DIR_PATH + "/refactored/refactored_image_gaussian.png");
 
     std::cout << "\nImage refactoring finished!" << std::endl;
     return 0;
